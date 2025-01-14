2680825bd6bf971b6efc1818cd3f694f9054a00c
Andreas Erbslöh <andreas.erbsloeh@uni-due.de>
2025-01-14 16:32:25 +0100

    Added examples AD5141 and AD5142

---
+++ examples/AD5141_SPI/main_ad5141_spi.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+
+#include "lib/spi_handler.h"
+#include "lib/ad5141_spi.h"
+
+
+int main(){ 
+    ad5141_spi_t setting_device = {
+        .spi_handler = &DEVICE_SPI_DEFAULT,
+        .device_csn = PICO_DEFAULT_SPI_CSN_PIN,
+        .init_done
+    }
+
+    // Init of device
+    ad5141_spi_init(&setting_device);
+
+    //Main Loop for communication
+    uint8_t pos = 0;
+    while (true){
+        ad5141_spi_define_level(&setting_device, 0, pos);
+        pos++;
+        sleep_ms(250);
+    };
+}
