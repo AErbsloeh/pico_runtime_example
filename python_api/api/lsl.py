fix: Moved calculation of DAQ update rate into hardware (stable for 2h, than freeze)

python_api/api/lsl.py
@@ -76,7 +76,7 @@ def start_stream_utilization(name: str, event_func: Event, sampling_rate: float=
 def record_stream(name: str, path2save: Path | str, event_func: Event, take_sample: list=(), subtract_channel: int=0) -> None:
     """Function for recording and saving the data pushed on LSL stream
     :param name:                String with name of the LSL stream in order to catch it
-    :param path2save:           Path to save the data (if string, it will be auto-converted)
+    :param path2save:           Path to save the data (if it is a string, it will be auto-converted)
     :param event_func:          Threading Event manager to control while loop
     :param take_sample:         List with indexes to take from LSL pull list for further processing
     :param subtract_channel:    Integer with number of channels to subtract from data stream
@@ -107,30 +107,33 @@ def record_stream(name: str, path2save: Path | str, event_func: Event, take_samp
         data_dset = f.create_dataset("data", (0, channels), maxshape=(None, channels), dtype="uint16")
         ts_dset.attrs["unit"] = ""
 
-        process_list = take_sample if len(take_sample) > 0 else [i for i in range(channels)]
-
         idx = 0
+        process_list = take_sample if len(take_sample) > 0 else [i for i in range(channels)]
         while event_func.is_set():
-            # Processing
-            sample, ts = inlet.pull_sample(timeout=FOREVER)
-            if sample is None:
+            samples, ts = inlet.pull_chunk(
+                max_samples=int(sampling_rate),
+                timeout=0.0
+            )
+            if samples is None:
                 continue
-            # Write into file
-            ts_dset.resize((idx + 1,))
-            ts_dset[idx] = ts if mode_util else 1e-6 * sample[0]
-            data_dset.resize((idx + 1, channels))
-            data_dset[idx] = [sample[i] for i in process_list]
-            idx += 1
-            f.flush()
+            else:
+                ts_dset.resize((idx+len(ts),))
+                data_dset.resize((idx + len(ts), channels))
+                for id0, (sample, tb) in enumerate(zip(samples, ts)):
+                    ts_dset[idx+id0] = tb if mode_util else 1e-6 * sample[0]
+                    data_dset[idx+id0] = [sample[i] for i in process_list]
+                idx += len(ts)
+                f.flush()
 
 
-def start_live_plotting(name: str, event_func: Event, take_samples: list=(), window_length: float=10., subtract_channel:int=0) -> None:
+def start_live_plotting(name: str, event_func: Event, take_samples: list=(), window_length: float=10., subtract_channel:int=0, update_rate: float=5.) -> None:
     """Function for LSL to enable live plotting of the incoming results
     :param name:            String with name of the LSL stream to get data
     :param event_func:      Event manager from Threading package for controlling while loop
     :param take_samples:    List with taking idx from LSL pulling event
     :param window_length:   Floating value with length of time window for plotting in seconds
     :param subtract_channel:Integer with number of channels to subtract from data stream
+    :param update_rate:     Floating value with update rate of the plot
     :return:                None
     """
     line_color = ['red', 'green', 'blue', 'lime']
@@ -140,7 +143,7 @@ def start_live_plotting(name: str, event_func: Event, take_samples: list=(), win
     channels = inlet.info().channel_count() - (subtract_channel if not mode_util else 0)
     sampling_rate = inlet.info().nominal_srate()
     if sampling_rate > 2000.:
-        raise AttributeError(f"Sampling rate {sampling_rate} is too high. This leads to instability with LSL.pull_sample()")
+        raise AttributeError(f"Sampling rate {sampling_rate} is too high")
 
     # --- Build app
     canvas = scene.SceneCanvas(
@@ -155,43 +158,70 @@ def start_live_plotting(name: str, event_func: Event, take_samples: list=(), win
     curves = list()
     for idx in range(channels):
         curves.append(scene.Line(
+            width=2,
             color=line_color[idx % len(line_color)],
-            parent=view.scene,
-            width=2
+            parent=view.scene
         ))
+    del idx
     # --- Build ring buffer and update func
-    number_samples = int(window_length * sampling_rate)
-    buffer_time = deque(maxlen=number_samples)
-    buffer_sigs = [deque(maxlen=number_samples) for _ in range(channels)]
-    def update(event):
+    number_samples_pull = int(sampling_rate / update_rate)
+    number_samples_window = int(window_length * sampling_rate)
+    buffer_time = deque(maxlen=number_samples_window)
+    buffer_sigs = [deque(maxlen=number_samples_window) for _ in range(channels)]
+
+    def update_util(event):
         # Getting data
-        samples, tb = inlet.pull_sample(timeout=FOREVER)
-        if mode_util:
-            buffer_time.extend([tb])
-            for idx in range(channels):
-                buffer_sigs[idx].extend([samples[idx]])
-        else:
-            buffer_time.extend([1e-6*samples[0]])
-            for idx in range(channels):
-                buffer_sigs[idx].extend([samples[take_samples[idx]]])
+        samples, tb = inlet.pull_sample(timeout=0.0)
+        if samples is None:
+            return
+        buffer_time.extend([tb])
+        for idx0 in range(channels):
+            buffer_sigs[idx0].extend([samples[idx0]])
         if not event_func.is_set():
+            canvas.close()
             app.quit()
-        if len(buffer_time) < 2 or samples is None:
+        if len(buffer_time) < 2:
             return
         # Process for data for plot
-        x = np.asarray(buffer_time)
-        for idx, curve in enumerate(curves):
-            y = np.asarray(buffer_sigs[idx])
+        x = np.asarray(buffer_time, dtype=np.float32)
+        for idx1, curve in enumerate(curves):
+            y = np.asarray(buffer_sigs[idx1], dtype=np.float32)
+            curve.set_data(np.column_stack((x, y)))
+        view.camera.set_range(
+            x=(x.min(), x.max()),
+            y=(0, 100)
+        )
+    def update_data(event):
+        # Getting data
+        samples = inlet.pull_chunk(
+            max_samples=number_samples_pull,
+            timeout=0.0
+        )[0]
+        if samples is None:
+            return
+        for sample in samples:
+            buffer_time.extend([1e-6*sample[0]])
+            for idx0 in range(channels):
+                buffer_sigs[idx0].extend([sample[take_samples[idx0]]])
+        if not event_func.is_set():
+            canvas.close()
+            app.quit()
+        if len(buffer_time) < 2:
+            return
+        # Process for data for plot
+        x = np.asarray(buffer_time, dtype=np.float32)
+        for idx1, curve in enumerate(curves):
+            y = np.asarray(buffer_sigs[idx1], dtype=np.float32)
             curve.set_data(np.column_stack((x, y)))
         # TODO: Add dynamic y-axis scaling
         view.camera.set_range(
             x=(x.min(), x.max()),
-            y=(0, 100) if mode_util else (0, 512)
+            y=(0, 65536)
         )
     # --- Starting the process
     app.Timer(
         interval='auto',
-        connect=update,
+        connect=update_util if mode_util else update_data,
         start=True
     )
     app.run()