11a3e0b098ac80fc0fbe65257128e24ae9ce53e8
Andreas Erbslöh <andreas.erbsloeh@uni-due.de>
2026-01-13 14:41:45 +0100

    fix: Add test data for DataAPI

--- python_api/api/data_api_test.py
+++ python_api/api/data_api_test.py
@@ -10,19 +10,30 @@ def path():
 
 
 def test_loader_data(path: Path):
-    dut = DataAPI(path, data_prefix='mock')
+    dut = DataAPI(path)
     data = dut.read_data_file(0)
     file = dut.get_file_name_data(0)
-    assert 'mock' in file
+    assert 'data' in file
     assert type(data) == RawRecording
+
+    assert data.sampling_rate == 2000.
+    assert data.num_channels == 3
+    assert data.num_channels == data.data.shape[0]
+    assert data.time.size == data.data.shape[1]
 
 
 def test_loader_util(path: Path):
-    dut = DataAPI(path, data_prefix='mock')
+    dut = DataAPI(path)
     data = dut.read_utilization_file(0)
     file = dut.get_file_name_util(0)
     assert 'util' in file
     assert type(data) == RawRecording
+
+    print(data)
+    assert data.sampling_rate == 2.
+    assert data.num_channels == 2
+    assert data.num_channels == data.data.shape[0]
+    assert data.time.size == data.data.shape[1]
 
 
 if __name__ == "__main__":
